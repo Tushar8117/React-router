e9a53fad97a149c66cdde1420cb21543d62fedd6
TusharD <tushar937297@gmail.com>
2024-03-05 23:56:50 +0530

    first commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+   const data = useLoaderData();
+
+    // const [data , setData] = useState([]) 
+    // useEffect(()=> {
+    //   fetch('https://api.github.com/users/tushar8117')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //   })
+    // },[])
+  return (
+    <div className='bg-gray-600 text-white text-3xl p-4'> Github Followers:
+    {data.followers}
+    <img src={data.avatar_url} alt='Git' width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+ export const  githubinfoloader = async () => {
+  const response = await fetch('https://api.github.com/users/tushar8117')
+
+  return(
+    response.json()
+    
+    ) 
+
+}
